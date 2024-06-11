837097c8225e68d0a6ec333f4a74296185ae4513
msvalandro <msvalandro@gmail.com>
2024-06-11 20:54:44 +0100

    feat: add logic to create block

--- __tests__/block.spec.ts
+++ __tests__/block.spec.ts
@@ -2,19 +2,25 @@ import { Block } from '../src/lib/block'
 
 describe('Block tests', () => {
   it('should be valid', () => {
-    const block = new Block(1, 'hash1')
+    const block = new Block(1, 'hash1', 'Block 1')
 
     expect(block.isValid()).toBe(true)
   })
 
   it('should not be valid due to index', () => {
-    const block = new Block(-1, 'hash1')
+    const block = new Block(-1, 'hash1', 'Block 1')
 
     expect(block.isValid()).toBe(false)
   })
 
-  it('should not be valid due to hash', () => {
-    const block = new Block(1, '')
+  it('should not be valid due to previous hash', () => {
+    const block = new Block(1, '', 'Block 1')
+
+    expect(block.isValid()).toBe(false)
+  })
+
+  it('should not be valid due to data', () => {
+    const block = new Block(1, '', '')
 
     expect(block.isValid()).toBe(false)
   })
@@ -24,6 +30,8 @@ describe('Block tests', () => {
 
     expect(block.isValid()).toBe(true)
     expect(block.getIndex()).toBe(0)
-    expect(block.getHash()).toBe('0000000000000000')
+    expect(block.getHash()).toBe(
+      'e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855',
+    )
   })
 })
--- src/lib/block.ts
+++ src/lib/block.ts
@@ -1,22 +1,28 @@
+import sha256 from 'crypto-js/sha256'
+
 export class Block {
   private index: number
   private hash: string
+  private previousHash: string
+  private data: string
+  private timestamp: number
 
-  constructor(index: number, hash: string) {
+  constructor(index: number, previousHash: string, data: string) {
     this.index = index
-    this.hash = hash
+    this.hash = this.generateHash()
+    this.previousHash = previousHash
+    this.data = data
+    this.timestamp = Date.now()
   }
 
   static genesis() {
-    return new Block(0, '0000000000000000')
+    return new Block(0, '0000000000000000', 'Genesis Block')
   }
 
-  isValid() {
-    if (this.index < 0 || !this.hash) {
-      return false
-    }
-
-    return true
+  private generateHash() {
+    return sha256(
+      this.index + this.data + this.timestamp + this.previousHash,
+    ).toString()
   }
 
   getIndex() {
@@ -25,5 +31,13 @@ export class Block {
 
   getHash() {
     return this.hash
+  }
+
+  isValid() {
+    if (this.index < 0 || !this.previousHash || !this.data) {
+      return false
+    }
+
+    return true
   }
 }
